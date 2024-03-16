c9628dc913ff5f7b1a8729920aab5fc20e0cb887
Timur Badretdinov <accounts@destiner.io>
2024-03-16 17:40:59 +0000

    Fix import resolution in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import path from 'path';
+
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import VueDevTools from 'vite-plugin-vue-devtools';
@@ -5,4 +9,11 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tsconfigPaths(), vue(), VueDevTools()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
