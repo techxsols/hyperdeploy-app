2d0d1263d1bd122faa04d08e215619132868b549
Timur Badretdinov <accounts@destiner.io>
2024-03-17 02:15:45 +0000

    Add AccountButton button

---
+++ src/components/AccountButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <span v-if="isAuthorized">{{ nickname }}</span>
+    <span v-else>Log in</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import useAccount from '@/composables/useAccount';
+
+const { isAuthorized, nickname } = useAccount();
+</script>
